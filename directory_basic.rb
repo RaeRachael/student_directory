fcdd56e323c374d36c412ddaa48f65e3282087ba
Rachael Ewins <rachaelewins@hotmail.com>
2020-05-04 14:58:19 +0000

    Let's commit the code (double check that it works as before).

--- directory_basic.rb
+++ directory_basic.rb
@@ -1,47 +1,58 @@
 
+@students = []
+
 def print_header    
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print_list(array2d)
+def print_list(array2d = @students)
   array2d.each { |student| puts "#{student[:name]}, (#{student[:cohort]} cohort)" }
 end
 
-def print_footer(list)
-  puts "Overall we have #{list.count} students"
+def print_footer(list = @students)
+  puts "Overall we have #{list.count} students\n"
 end
 
 def input_students
   puts "Enter the name of students (one per line)"
   puts "To finish enter a black line"
-  students = []
   name =gets.chomp
   while !name.empty?
-    students << { :name => name, :cohort => :november}
-    puts "We now have #{students.count} students\n"
+    @students << { :name => name, :cohort => :november}
+    puts "We now have #{@students.count} students\n"
     name = gets.chomp
   end
-  students
+end
+
+def process(user_choice)
+  case user_choice
+  when 1.to_s
+    input_students
+  when 2.to_s
+    output_students
+  when 9.to_s
+    exit
+  end
 end
 
 def interactive_menu
-  students = [] 
   loop do
-    list_of_options= ["1, input students", "2, view students", "9, exit"]
-    puts list_of_options
-    case gets.chomp
-    when 1.to_s
-      students = input_students
-    when 2.to_s
-      print_header
-      print_list(students)
-      print_footer(students)
-    when 9.to_s
-      break
-    end
+    menu_options
+    process(gets.chomp)
   end
 end
-    
+
+def menu_options
+  list_of_options = ["1, input students", "2, view students", "9, exit"]
+  puts list_of_options
+end
+
+def output_students
+  print_header
+  print_list_students
+  print_footer
+end 
+  
 interactive_menu
 
