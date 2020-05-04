b6b618b704630df437c14090a4156d098b80feaf
Rachael Ewins <rachaelewins@hotmail.com>
2020-05-04 14:16:23 +0000

    using #center for output of list

--- directory.rb
+++ directory.rb
@@ -1,15 +1,16 @@
 
 def print_header    
-    puts "The students of Villains Academy"
-    puts "-------------"
+    puts "The students of Villains Academy".center(40)
+    puts "-------------".center(40)
 end
 
 def print_list(array2d)
-    array2d.each { |student| puts "#{student[:name]}, (#{student[:cohort]} cohort)" }
+    array2d.each { |student|
+        puts "#{student[:name]}, (#{student[:cohort]} cohort)".center(40) }
 end
 
 def print_footer(list)
-    puts "Overall we have #{list.count} students"
+    puts "Overall we have #{list.count} students".center(40)
 end
 
 def input_students
