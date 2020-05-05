89450c982f684ea95706b977e9aa69d531a9002e
Rachael Ewins <rachaelewins@hotmail.com>
2020-05-05 10:53:51 +0000

    refractoring and adding options for save and load locations

--- directory_basic.rb
+++ directory_basic.rb
@@ -19,7 +19,7 @@ def input_students
   puts "To finish enter a black line"
   name = STDIN.gets.chomp
   while !name.empty?
-    @students << { :name => name, :cohort => :november}
+    push_to_array(name)
     puts "We now have #{@students.count} students\n"
     name = STDIN.gets.chomp
   end
@@ -33,9 +33,11 @@ def process(user_choice)
     output_students
   when 3.to_s
     save_data
+    puts "data saved"
   when 4.to_s
     load_data
   when 9.to_s
+    puts "program exiting"
     exit
   end
 end
@@ -63,7 +65,8 @@ def output_students
 end
 
 def save_data
-  file = File.open("students.csv", "w")
+  filename = filename_choice
+  file = File.open(filename, "w")
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     file.puts student_data.join(",")
@@ -71,28 +74,54 @@ def save_data
   file.close
 end
 
-def load_data(filename = "students.csv")
+def load_data()
+  filename = filename_choice
+  file_exist_check(filename)
+  read_data_from(filename)
+  count_loaded_students_from(filename)
+end
+
+def read_data_from(filename)
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
-    @students << {:name => name, :cohort => cohort}
+    push_to_array(name, cohort)
   end
   file.close
 end
 
-def load_existing_file
-  filename = ARGV.first
+def load_from_existing_file
+  filename = ARGV.first || "students.csv" 
   return if filename == nil
-  if File.exists?(filename)
-    load_data(filename)
-    puts "loaded #{@students.count} from #{filename}"
-  else
+  file_exist_check(filename)
+  read_data_from(filename)
+  count_loaded_students_from(filename)
+end
+  
+def count_loaded_students_from(filename)
+  puts "loaded #{@students.count} from #{filename}"
+end
+
+def push_to_array(name, cohort = :november)
+  @students << { :name => name, :cohort => cohort.to_sym }
+end
+
+def filename_choice
+  puts "enter filename, no filename entry will pick 'students.csv'"
+  filename = gets.chomp
+  filename = "students.csv" if filename == ""
+  file_exist_check(filename)
+  filename
+end
+
+def file_exist_check(filename)
+  if !File.exists?(filename)
     puts "#{filename} does not exist, exiting program"
     exit
   end
 end
 
-load_existing_file
+load_from_existing_file
 
 interactive_menu
 
